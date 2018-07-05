1d5cee7d1b2ec1d76ad8f02236553017286c49b5
Halil Özel <halilozel1903@gmail.com>
2018-07-05 19:25:56 +0300

    break ve continue dersleri eklendi.

---
+++ src/Break.kt
@@ -0,0 +1,34 @@
+
+
+/*
+  Break :   Döngüden çıkılmasını sağlayan bir komuttur.
+
+ */
+
+
+
+fun main(args: Array<String>) {
+
+
+    for(i in 1..10){ // 1 den 10'a kadar dön
+
+        if (i == 8) // i değeri 8'e eşitse
+            break // kır ve döngüden çık
+
+     println(i) // i değerlerini ekranda göster.
+    }
+
+
+
+    for (char in "Halil&Özel"){ // String bir ifade
+
+        if (char == '&') // bu karakteri gördüğünde
+            break // kır ve döngüden çık
+        print(char) // break olmadan önceki değeri ekranda göster
+    }
+
+
+
+
+
+}
