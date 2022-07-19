c7f9c4359dbeea7e767501a31bfb8551661c7b0a
Halil Özel <halilozel1903@gmail.com>
2022-07-19 19:32:03 +0300

    Constructor lesson was added.

---
+++ src/main/kotlin/Constructor.kt
@@ -0,0 +1,13 @@
+/*
+In Kotlin, constructor is a block of code similar to method. Constructor is declared with the same name
+as the class followed by parenthesis '()'. Constructor is used to initialize the variables at the time of object creation.
+ */
+
+class Student(val name: String, var id: Int)
+
+fun main() {
+    val student = Student("Halil", 34)
+
+    println("Name = ${student.name}")
+    println("Id = ${student.id}")
+}
