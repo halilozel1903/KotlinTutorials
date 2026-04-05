3ef575a561b79029da91e2a2ffc44471f2210221
Halil Ozel <halilozel1903@gmail.com>
2026-04-05 18:29:39 +0300

    Update main function to call lesson functions for improved execution flow

--- src/main/kotlin/modern2026/Modern2026Runner.kt
+++ src/main/kotlin/modern2026/Modern2026Runner.kt
@@ -2,9 +2,11 @@ package modern2026
 
 fun main() {
     println("=== Kotlin 2026 Modern Lessons ===")
-    println("Run each lesson file directly from IntelliJ for focused learning:")
-    println("1) SealedAndDataObject2026.kt")
-    println("2) ValueClassAndTypeSafety2026.kt")
-    println("3) ResultAndScopeFunctions2026.kt")
+
+    lessonSealedAndDataObject2026()
+    lessonValueClassAndTypeSafety2026()
+    lessonResultAndScopeFunctions2026()
+    lessonCoroutinesFlow2026()
+    lessonSerialization2026()
 }
 
